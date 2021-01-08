d4b406c5d716467245ecfc163c8bcb36804201db
David Freina <david.freina@student.uibk.ac.at>
2021-01-08 20:41:26 +0100

    added variable for flatted array

--- project/analyze_frames_input_preprocessing/index.js
+++ project/analyze_frames_input_preprocessing/index.js
@@ -1,3 +1,4 @@
 exports.handler = (event, context, callback) => {
-    return callback(null, {"extractedFramesSplitFoldersCombined": event.extractedFramesSplitFoldersCombined.flat(), "extractedFramesSplitFoldersNumber": event.extractedFramesSplitFoldersCombined.flat().length});
+    let flatArray = event.extractedFramesSplitFoldersCombined.flat()
+    return callback(null, {"extractedFramesSplitFoldersCombined": flatArray, "extractedFramesSplitFoldersNumber": flatArray.length});
 };
